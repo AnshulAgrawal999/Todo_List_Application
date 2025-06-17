d86f4890f5e84dd535490de5127e66fba101301f
anshul <anshulagr799@gmail.com>
2025-06-17 13:52:30 +0530

    add PORT number

--- todo-list-express-server/index.js
+++ todo-list-express-server/index.js
@@ -7,7 +7,7 @@ dotenv.config() ;
 
 const cors = require( 'cors' ) ;
 
-const PORT = process.env.PORT || 6000 ;
+const PORT = process.env.PORT || 4000 ;
 
 const { connection } = require( './configs/db' ) ;
 
